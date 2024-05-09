2844d888ff49d7309930ae9eef4f9702b652e282
reecewdoyle <144601273+reecewdoyle@users.noreply.github.com>
2024-05-09 19:49:31 +1000

    feat: parent structure in place

--- src/components/SocialCardForm.jsx
+++ src/components/SocialCardForm.jsx
@@ -1,27 +1,28 @@
 import React from "react";
 
 export default class SocialCardForm extends React.Component{
-    constructor(){
-        super();
+	constructor(){
+		super();
 
-        this.state = {
-            content: "Default post content",
-            imageUrl: ""
-        }
-    }
+		this.state = {
+			content: "Default post content",
+			imageUrl: ""
+		}
+	}
 
-    render(){
-        return (
-            <section className="socialCardForm">
-                <label htmlFor="postContent">Post content:</label>
-                <input 
-                    type="text" 
-                    name="postContent" 
-                    className="postContent" 
-                    value={this.state.content}
-					 onChange={(event) => {this.setState({content: event.target.value})}}
-                    ></input>
-            </section>
-        )
-    }
+	render(){
+		return (
+			<section className="socialCardForm">
+				<label htmlFor="postContent">Post content:</label>
+				<input 
+					type="text" 
+					name="postContent" 
+					className="postContent" 
+					value={this.state.content} 
+					onChange={(event) => {this.setState({content: event.target.value})}}
+				/>
+                <button onClick={this.props.toggleEditMode}>Toggle Edit Mode</button>
+			</section>
+		)
+	}
 }
